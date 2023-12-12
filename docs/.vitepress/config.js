da91a01f4f8090896d9d427f652e65156c228f77
wfnuser <wfnuser@hotmail.com>
2023-12-12 12:01:09 +0800

    enable markdown rendering by import mathjax

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,13 @@
+import mathjax3 from "markdown-it-mathjax3";
+
 module.exports = {
   title: "编程语言理论完全笔记",
   base: "/plt/",
+  markdown: {
+    config: (md) => {
+      md.use(mathjax3);
+    },
+  },
   themeConfig: {
     nav: [
       { text: "软件基础(idris)", link: "/software-foundation/" },
